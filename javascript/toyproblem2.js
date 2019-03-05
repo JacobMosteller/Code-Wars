b37bd5ee400b514f34cd9d4f6cf473a5e096a417
Jacob Mosteller <jacobm@mostellerllc.com>
2019-03-04 18:12:01 -0700

    daily toyproblem 2

---
+++ javascript/toyproblem2.js
@@ -0,0 +1,6 @@
+var botleOfCokeZero = {
+    content: .25,
+    liquid: "Coke Zero",
+    fluidOz: 16.9,
+    ingredients: ['Carbonated water', 'caramel color', 'phosphoric acid', 'aspartame', 'potassium benzoate', 'natural flavors', 'potassium citrate', 'acesulfame potassium', 'caffeine']
+}
