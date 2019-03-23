dffcef1d6b2411576383d49f0a5deae82a3b7199
Jacob Mosteller <jacobm@mostellerllc.com>
2019-03-23 09:40:02 -0600

    finnished problem 10

---
+++ toyproblem10.js
@@ -0,0 +1,17 @@
+function fizzbuzz(n) {
+    const output = [];
+    for (let i = 1; i <= n; ++i) {
+        const multipleOf3 = !(i % 3);
+        const multipleOf5 = !(i % 5);
+        if (multipleOf3) {
+            if (multipleOf5)
+                output.push("FizzBuzz");
+            else
+                output.push("Fizz");
+        } else if (multipleOf5)
+            output.push("Buzz");
+        else
+            output.push(i);
+    }
+    return output;
+}
